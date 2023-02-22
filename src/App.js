8771010120a7de5c4ba7c5b1fcbda5c8f2cbc82c
biniyam <aregubini@gmail.com>
2023-02-22 08:13:09 +0300

    added pp gateway with fixed amount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 function App() {
+  const Paypal = () => {
+    return (
+      <PayPalScriptProvider
+        options={{
+          "client-id": `${process.env.EQUB_PUBLIC_PAYPAL_CLIENT_ID}`,
+        }}
+      >
+        <PayPalButtons
+          style={{
+            shape: "pill",
+            color: "blue",
+            layout: "vertical",
+            label: "paypal",
+          }}
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: 20,
+                  },
+                },
+              ],
+            });
+          }}
+          onApprove={async (data, actions) => {
+            const order = await actions.order.capture();
+            console.log({ order: order.id });
+            // const response = await axiosApp.post(
+            //   "/checkout-paypal/",
+            //   {
+            //     id: order.id,
+            //   },
+            //   {
+            //     headers: {
+            //       Authorization: `Bearer ${localStorage.getItem(
+            //         BINGO_ACCESS_TOKEN_KEY
+            //       )}`,
+            //     },
+            //   }
+            // );
+            // console.log(response);
+          }}
+          onError={(err) => {
+            console.log(err);
+          }}
+        />
+      </PayPalScriptProvider>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paypal />
     </div>
   );
 }
